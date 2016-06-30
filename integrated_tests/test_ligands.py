bf18eaf6f070fea72cf478265bcee867bd03175c
samirelanduk <sam.ireland.uk@gmail.com>
2016-06-30 13:24:08 +0100

    Integrated ligand retrieval tests

--- integrated_tests/test_ligands.py
+++ integrated_tests/test_ligands.py
@@ -14,3 +14,21 @@ class LigandAccessTests(TestCase):
     def test_cannot_get_ligand_by_incorrect_id(self):
         with self.assertRaises(NoSuchLigandError):
             pygtop.get_ligand_by_id(0)
+
+
+    def test_can_get_ligand_by_name(self):
+        ligand = pygtop.get_ligand_by_name("paracetamol")
+        self.assertIsInstance(ligand, Ligand)
+        self.assertEqual(ligand.name(), "paracetamol")
+
+
+    def test_cannot_get_ligand_by_incorrect_name(self):
+        with self.assertRaises(NoSuchLigandError):
+            pygtop.get_ligand_by_name("paracetamoxyfrusebendroneomycin")
+
+
+    def test_can_get_all_ligands(self):
+        ligands = pygtop.get_all_ligands()
+        self.assertIsInstance(ligands, list)
+        self.assertIsInstance(ligands[0], Ligand)
+        self.assertGreater(len(ligands), 5000)
