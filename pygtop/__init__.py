fcd73d65ce63525b79c10c7286b367435b6880ed
Sam Ireland <sam.ireland.uk@gmail.com>
2016-07-06 11:55:29 +0100

    Remove old text

--- pygtop/__init__.py
+++ pygtop/__init__.py
@@ -1,6 +1,7 @@
 from .ligands import *
 from .targets import *
 from .interactions import *
+from .exceptions import *
 
 __version__ = "1.1.0"
 __author__ = "Sam Ireland"
--- pygtop/exceptions.py
+++ pygtop/exceptions.py
@@ -28,14 +28,6 @@ class NoSuchInteractionError(Exception):
 
 
 
-class PropertyNotRequestedYetError(Exception):
-    """The exception raised if a ligand or target's property is accessed before
-    it has been explictly requested. If the property is not one which can be
-    accessed from the web services, a standard AttributeError will be returned."""
-    pass
-
-
-
 class NoSuchTypeError(Exception):
     """The exception raised if a random ligand or target is requested of a type
     which does not exist."""
